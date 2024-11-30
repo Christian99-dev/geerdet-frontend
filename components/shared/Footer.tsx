af2fe3ba0b5a8d04a9f75c416f7434b422813e3f
Christian Keller <k.christian9@web.de>
2024-11-30 10:49:13 +0100

    feat: footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+import Link from "./Link";
+
 export const Footer = () => {
-  return <section className="bg-white">Footer</section>;
+  return (
+    <section className="p-[6em_0_4em_0] text-center bg-background">
+      <div className="flex justify-center gap-8 mb-12">
+        <Link text="IMPRESSUM" href="#" />
+        <Link text="Haftungsausschluss" href="#" />
+        <Link text="Datenschutzerklärung" href="#" />
+      </div>
+
+      <p className="flex justify-center gap-4 uppercase text-gray-50 tracking-wide text-9">
+        © 2024 | geerdet, Iris Keller-Hoenisch | Alle Rechte vorbehalten
+      </p>
+    </section>
+  );
 };
--- components/shared/Link.tsx
+++ components/shared/Link.tsx
@@ -1,7 +1,7 @@
 const Link = ({ text, href }: { text: string; href: string }) => {
   return (
     <a
-      className="text-9 ease-default transition duration-default text-gray-50 border-b-[2px] border-dotted hover:text-white hover:border-b-transparent"
+      className="uppercase text-9 ease-default transition duration-default text-gray-50 border-b-[2px] border-dotted hover:text-white hover:border-b-transparent"
       href={href}
     >
       {text}
