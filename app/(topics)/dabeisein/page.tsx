9c2103cd2a317a3079f6d44560ed636b81e18af4
Christian <k.christian9@web.de>
2025-03-24 21:13:16 +0100

    fix: enhance text styling and structure for improved readability

--- app/(topics)/dabeisein/page.tsx
+++ app/(topics)/dabeisein/page.tsx
@@ -16,33 +16,41 @@ export default async function Datenschutz() {
     <Layout title={pageTitle} img={img}>
       <h2>Dabeisein ist alles - Du bist gefragt</h2>
       <p>
-        Du willst raus aufs Feld? Wir laden Dich herzlich ein, auch Deine Hände
-        in die Erde zu stecken! Sprich uns einfach an und sei nächstes Mal
-        dabei. Hilf mit zu pflanzen, zu pflegen und zu ernten. Wir treffen uns
-        zurzeit immer freitags und samstags.
+        <span>Du willst raus aufs Feld?</span> Wir laden Dich herzlich ein, auch
+        Deine Hände in die Erde zu stecken! Sprich uns einfach an und sei
+        nächstes Mal dabei.{" "}
+        <span>Hilf mit zu pflanzen, zu pflegen und zu ernten</span>. Wir treffen
+        uns zurzeit immer freitags und samstags.
       </p>
       <p>
-        Du kennst andere, die das auch interessiert? Melde Dein Team, Deinen
-        Verein oder Deine Klasse einfach an. Ihr helft ein paar Stunden mit,
-        lernt dabei, wie regenerative Landwirtschaft funktioniert und testet,
+        <span>Du kennst andere, die das auch interessiert?</span> Melde Dein
+        Team, Deinen Verein oder Deine Klasse einfach an. Ihr helft ein paar
+        Stunden mit, lernt dabei,{" "}
+        <span>wie regenerative Landwirtschaft funktioniert</span> und testet,
         wie Obst und Gemüse schmeckt, wenn es frisch geerntet wird!
       </p>
       <p>
-        Du hast Lust, Dich um Hühner, Laufenten, Ziegen oder Schafe zu kümmern?
-        Wir sind dabei. Haltung gefährdeter Haustierrassen liegt uns am Herzen.
-        Wir suchen verantwortungsvolle Leute, die dafür mit ins Team kommen.
+        <span>
+          Du hast Lust, Dich um Hühner, Laufenten, Ziegen oder Schafe zu
+          kümmern?{" "}
+        </span>
+        Wir sind dabei. Haltung <span>gefährdeter Haustierrassen</span> liegt
+        uns am Herzen. Wir suchen verantwortungsvolle Leute, die dafür mit ins
+        Team kommen.
       </p>
       <p>
-        Du liebst es, Gemüse einzukochen und zu fermentieren? Du möchtest
-        Kräuter trocknen und verarbeiten, z. B. zu Tee, Salben oder Seife? Dann
-        lass uns unbedingt über Zusammenarbeit reden! Du hast Lust so etwas zu
-        lernen? Wir bauen eine Lern-Community auf.
+        <span>Du liebst es, Gemüse einzukochen und zu fermentieren?</span> Du
+        möchtest
+        <span>Kräuter trocknen und verarbeiten</span>, z. B. zu Tee, Salben oder
+        Seife? Dann lass uns unbedingt über Zusammenarbeit reden! Du hast Lust
+        so etwas zu lernen? Wir bauen eine Lern-Community auf.
       </p>
       <p>
-        Du möchtest uns unterstützen? Wir freuen uns über Spenden. Damit
-        beschleunigst Du den Aufbau unserer Arbeit. Wir kaufen davon Bäume,
-        Sträucher, Zäune und Saatgut. Pflanzen aus Deinem Garten, die Du nicht
-        mehr benötigst, finden bei uns ein neues Zuhause.
+        <span>Du möchtest uns unterstützen?</span> Wir freuen uns über{" "}
+        <span>Spenden</span>. Damit beschleunigst Du den Aufbau unserer Arbeit.
+        Wir kaufen davon Bäume, Sträucher, Zäune und Saatgut. Pflanzen aus
+        Deinem Garten, die Du nicht mehr benötigst, finden bei uns ein neues
+        Zuhause.
       </p>
     </Layout>
   );
--- app/(topics)/solawi/page.tsx
+++ app/(topics)/solawi/page.tsx
@@ -14,42 +14,50 @@ export default async function Datenschutz() {
 
   return (
     <Layout title={pageTitle} img={img}>
-      <h2>Solidarische Landwirtschaft - und wie sie bei uns funktioniert</h2>
-      <p>Was ist eigentlich eine SoLawi?</p>
+      <h3>Was ist eigentlich eine SoLawi?</h3>
       <p>
         Solidarische Landwirtschaft (SoLaWi) heißt: Ein landwirtschaftlicher
         Betrieb wird von der Gemeinschaft getragen. Weil es lokal eine
         Verbrauchergemeinschaft gibt, die finanzielle Mitverantwortung
-        übernimmt, kann unser Betrieb marktunabhängig Lebensmittel anbauen. Bei
-        uns kaufst Du nicht einfach nur Gemüse, sondern bist Teil der Idee,
-        indem Du Deine Ernteanteile vorab bezahlst und - sofern es Zeit und
-        körperliche Konstitution erlauben - auf dem Feld mitanpackst.
+        übernimmt, kann unser Betrieb
+        <span> marktunabhängig Lebensmittel anbauen</span>. Bei uns kaufst Du
+        nicht einfach nur Gemüse, sondern bist Teil der Idee, indem Du Deine
+        Ernteanteile vorab bezahlst und - sofern es Zeit und körperliche
+        Konstitution erlauben - auf dem Feld mitanpackstF
       </p>
       <p>
-        Eine Gruppe von Einzelpersonen verpflichtet sich zur Abnahme
-        wöchentlicher Ernteanteile und zu Arbeitseinsätzen auf dem Feld. Als
-        Betrieb übernehmen wir die professionelle Anbauplanung, sind zuständig
-        für Pflege und Ernte der Pflanzen und gehen mit Grundinvestitionen in
-        Vorleistung. Diversität und Anzahl und Sorten des wöchentlichen Gemüses
-        schwanken erwartungsgemäß entsprechend der Jahreszeiten. So befindet
-        sich natürlich von Juli bis August die größte Vielfalt in der
-        wöchentlichen Kiste. Auch wetterbedingt können Erträge schwanken. Die
-        Gemeinschaft teilt sich die Ernte, aber auch das Anbaurisiko. Der
-        Betrieb hat durch die Vorauszahlung der monatlichen Beiträge finanzielle
-        Planungssicherheit. Durch die Abnahmegarantie kann er Überproduktion und
-        Lebensmittelverschwendung vermeiden.
+        Eine Gruppe von Einzelpersonen{" "}
+        <span>verpflichtet sich zur Abnahme</span> wöchentlicher Ernteanteile
+        und zu Arbeitseinsätzen auf dem Feld. Als Betrieb übernehmen wir die
+        professionelle Anbauplanung, sind zuständig für Pflege und Ernte der
+        Pflanzen und gehen mit Grundinvestitionen in Vorleistung.
       </p>
-      <h2>Was erwartet mich bei der SoLawi von geerdet und was kostet es?</h2>
+      <p>
+        Diversität und Anzahl und Sorten des wöchentlichen Gemüses schwanken
+        erwartungsgemäß entsprechend der Jahreszeiten. So befindet sich
+        natürlich von <span>Juli bis August die größte Vielfalt</span> in der
+        wöchentlichen Kiste. Auch wetterbedingt können Erträge schwanken. Die
+        Gemeinschaft teilt sich die Ernte, aber auch das Anbaurisiko.
+      </p>
+      <p>
+        Der Betrieb hat durch die Vorauszahlung der monatlichen Beiträge
+        finanzielle Planungssicherheit. Durch die Abnahmegarantie kann er{" "}
+        <span>Überproduktion und Lebensmittelverschwendung vermeiden.</span>
+      </p>
+      <h2>
+        Was erwartet mich bei der SoLawi von{" "}
+        <span className="geerdet">geerdet </span>und was kostet es?{" "}
+      </h2>
       <p>
         Indem Du ein Ernte-Abo bestellst, wirst du für ein Jahr Teil der
         Solidar-Gemeinschaft. Vor Beginn der Erntesaison beteiligst Du Dich
-        einmalig mit einem Saatanteil von 180,00€. Davon kaufen wir Saatgut,
-        Jungpflanzen, Pflanzerde, usw., die wir benötigen, um den Gemüseanbau an
-        den Start zu bringen.
+        <span> einmalig mit einem Saatanteil von 180,00€.</span> Davon kaufen
+        wir Saatgut, Jungpflanzen, Pflanzerde, usw., die wir benötigen, um den
+        Gemüseanbau an den Start zu bringen.
       </p>
       <p>
         In diesem Jahr, 2025 zahlst Du ab Mai/Juni nur noch den Betrag für die
-        Ernteanteile: 6 - 7 x 130,00 € /Monat und darfst Dich wöchentlich auf
+        Ernteanteile: 6 – 7 x 130,00 € /Monat und darfst Dich wöchentlich auf
         leckeres, ungespritztes Gemüse direkt aus Taunusstein freuen. Mit der
         Wochenernte in deiner Gemüsekiste kannst Du ca. 2 Personen versorgen.
         Von den monatlichen Beiträgen werden nach und nach die
@@ -65,25 +73,25 @@ export default async function Datenschutz() {
         verlängern (z. B. bis zur 50. Kalenderwoche).
       </p>
       <p>
-        Am wöchentlichen Abholtag kommst Du zum Bornhof, Dornbornstraße 10 in
-        Taunusstein-Wehen* und wiegst Dir Dein Gemüse ab. Zur Abholung bringst
-        Du Deine eigene Kiste, Korb oder Tasche mit. Im kleinen Hofladen kannst
-        Du hier neben Bio-Eiern und Bio-Rindfleisch noch weitere gesunde
-        Leckereien finden, die Deinen Speiseplan bereichern.
+        Am wöchentlichen Abholtag kommst Du zum{" "}
+        <span> Bornhof, Dornbornstraße 10 in Taunusstein-Wehen*</span> und
+        wiegst Dir Dein Gemüse ab. Zur Abholung bringst Du Deine eigene Kiste,
+        Korb oder Tasche mit. Im kleinen Hofladen kannst Du hier neben Bio-Eiern
+        und Bio-Rindfleisch noch weitere gesunde Leckereien finden, die Deinen
+        Speiseplan bereichern.
       </p>
       <img src="/images/bornhof.png"></img>
-      <h2>Wie kann ich mich sonst noch beteiligen?</h2>
+      <h2>Wie kann ich mich noch beteiligen?</h2>
       <p>
         Ja, Deine Hilfe ist gefragt! Du bist herzlich eingeladen. Es ist klar,
         dass unsere Felder nicht ganz allein von unseren Gemüsegärtnerinnen und
         -Gärtnern bearbeitet werden können. Im Vertrag zum Gemüse-Abo legst Du
-        selbst verbindlich fest, ob und wie oft Du mitarbeiten möchtest. Auf der
-        Seite „Termine und Events“* findest Du eine Übersicht, über das, was
-        gerade ansteht und wo Du tatkräftig mithelfen kannst. Hier trägst Du die
-        konkreten Zeiten ein. Solltest Du verhindert sein, trägst Du Dich drei
-        Tage vorher aus und informierst uns über info(ät)geerdet-taunusstein.de.
+        selbst verbindlich fest, ob und wie oft Du mitarbeiten möchtest. Melde
+        Dich einfach per E-Mail an info(ät)geerdet-taunusstein.de, wenn Du
+        mithelfen möchtest oder Fragen hast. Wir freuen uns auf Deine
+        Unterstützung!
       </p>
-      <p>Komm mit aufs Feld! – Zusammen macht es einfach Spaß.</p>
+      <p><span>Komm mit aufs Feld!</span> - Zusammen macht es einfach Spaß. </p>
     </Layout>
   );
 }
